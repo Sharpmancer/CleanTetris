ae202568519ecb73b6577eaed965235237c48dd6
selskiyvrach <selskiyvrach@gmail.com>
2025-10-17 16:34:02 +0400

    moved lifecycle control operation from runnablecontext into a dedicated class

---
+++ Assets/Tetris/Scripts/Libs/Bootstrap/LifecycleControlledObjects.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Libs.Core.Lifecycle;
+
+namespace Libs.Bootstrap
+{
+    internal class LifecycleControlledObjects
+    {
+        private readonly List<IPreInitializable> _preInitializables = new();
+        private readonly List<IInitializable> _initializables = new();
+        private readonly List<ITickable> _tickables = new();
+        private readonly List<IDisposable> _disposables = new();
+        
+        internal void Add(object instance)
+        {
+            if(instance is IPreInitializable preInitializable)
+                _preInitializables.Add(preInitializable);
+            if(instance is IInitializable initializable)
+                _initializables.Add(initializable);
+            if(instance is ITickable tickable)
+                _tickables.Add(tickable);
+            if(instance is IDisposable disposable)
+                _disposables.Add(disposable);
+        }
+
+        internal void Initialize()
+        {
+            foreach (var initializable in _initializables) 
+                initializable.Initialize();
+        }
+
+        internal void PreInitialize()
+        {
+            foreach (var initializable in _preInitializables.OrderBy(x => x.PreInitOrder)) 
+                initializable.PreInitialize();
+        }
+
+        internal void Tick(float timeDelta)
+        {
+            foreach (var tickable in _tickables) 
+                tickable.Tick(timeDelta);
+        }
+
+        internal void Dispose()
+        {
+            // disposing in reverse-init order
+            for (var i = _disposables.Count - 1; i >= 0; --i) 
+                _disposables[i].Dispose();
+        }
+    }
+}
--- Assets/Tetris/Scripts/Libs/Bootstrap/RunnableContext.cs
+++ Assets/Tetris/Scripts/Libs/Bootstrap/RunnableContext.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Libs.Core;
 
 namespace Libs.Bootstrap
@@ -8,64 +7,39 @@ namespace Libs.Bootstrap
     internal class RunnableContext : IInstallableContext
     {
         private readonly RunnableContext _parent;
-        private readonly Dictionary<Type, object> _instances = new();
-        private readonly List<IPreInitializable> _preInitializables = new();
-        private readonly List<IInitializable> _initializables = new();
-        private readonly List<ITickable> _tickables = new();
-        private readonly List<IDisposable> _disposables = new();
-
+        private readonly Dictionary<Type, object> _contracts = new();
+        private readonly LifecycleControlledObjects _runnables = new();
+        
         internal RunnableContext(RunnableContext parent = null) => 
             _parent = parent;
 
         public T Get<T>() where T : class =>  
-            _instances.TryGetValue(typeof(T), out var instance) 
+            _contracts.TryGetValue(typeof(T), out var instance) 
                 ? (T)instance 
-                : _parent?.Get<T>() ?? throw new KeyNotFoundException($"No binding for {typeof(T).Name} found");
+                : _parent?.Get<T>() ?? throw new KeyNotFoundException($"No implementation for {typeof(T).Name} found");
 
         public void RegisterContract<TContract>(TContract instance) => 
-            _instances.Add(typeof(TContract), instance);
+            _contracts.Add(typeof(TContract), instance);
 
-        public void RegisterRunnable(object instance)
-        {
-            if(instance is IInitializable initializable)
-                _initializables.Add(initializable);
-            if(instance is ITickable tickable)
-                _tickables.Add(tickable);
-            if(instance is IDisposable disposable)
-                _disposables.Add(disposable);
-            if(instance is IPreInitializable preInitializable)
-                _preInitializables.Add(preInitializable);
-        }
-
+        public void RegisterRunnable(object instance) => 
+            _runnables.Add(instance);
+        
         internal void RunInstallers(IEnumerable<Installer> installers)
         {
             foreach (var installer in installers) 
                 installer.Install(this);
         }
 
-        internal void RunPreInitializables()
-        {
-            foreach (var item in _preInitializables.OrderBy(n => n.Order)) 
-                item.PreInitialize();
-        }
+        internal void RunPreInitializables() => 
+            _runnables.PreInitialize();
 
-        internal void RunInitializables()
-        {
-            foreach (var initializable in _initializables) 
-                initializable.Initialize();
-        }
+        internal void RunInitializables() => 
+            _runnables.Initialize();
 
-        internal void RunTickables(float timeDelta)
-        {
-            foreach (var tickable in _tickables) 
-                tickable.Tick(timeDelta);
-        }
+        internal void RunTickables(float timeDelta) => 
+            _runnables.Tick(timeDelta);
 
-        // Disposes in reverse init order
-        internal void RunDisposables()
-        {
-            for (var i = _disposables.Count - 1; i >= 0; --i) 
-                _disposables[i].Dispose();
-        }
+        internal void RunDisposables() => 
+            _runnables.Dispose();
     }
 }
